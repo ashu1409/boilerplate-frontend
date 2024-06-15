b99f637bb69b2645a36fb92e008dc48a1c963519
Ashutosh Jha <65663701+ashu1409@users.noreply.github.com>
2024-06-15 17:08:38 +0530

    Create login.vue

---
+++ pages/login.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <form @submit.prevent="login">
+      <input type="email" v-model="email" placeholder="Email" />
+      <input type="password" v-model="password" placeholder="Password" />
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    async login() {
+      try {
+        await this.$axios.post('/login', {
+          email: this.email,
+          password: this.password,
+        });
+        this.$router.push('/payments');
+      } catch (error) {
+        console.error('Login failed', error);
+      }
+    },
+  },
+};
+</script>
